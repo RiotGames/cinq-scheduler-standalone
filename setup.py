6a11acd3c455ffd44df041cbfebbdc806d043fd6
Marty Chong <marty@themesh.ca>
2018-09-24 14:02:09 -0700

    Updating pkg version requirements

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     setup_requires=['setuptools_scm'],
     install_requires=[
-        'cloud_inquisitor~=2.0.0',
+        'cloud_inquisitor~=2.0',
         'APScheduler~=3.3',
     ],
     extras_require={
